054045c772d894e7eeff71db80cd267a451fd5c0
agamings21 <andrewharfst@hotmail.com>
2025-10-07 16:43:03 -0400

    feat: register sql provider

--- src/SqlFormattingProvider.ts
+++ src/SqlFormattingProvider.ts
@@ -1,41 +1,55 @@
-// import * as vscode from 'vscode';
+import * as vscode from 'vscode';
+import  FileProvider, * as fileProvider from './FileProvider';
 
-// export class SqlFormattingProvider implements vscode.DocumentFormattingEditProvider {
 
-//   provideDocumentFormattingEdits(
-//     document: vscode.TextDocument,
-//     formattingOptions: vscode.FormattingOptions,
-//   ): vscode.TextEdit[] {
-//     try {
-//       return [
-//         vscode.TextEdit.replace(
-//           this.fullDocumentRange(document),
-//           this.formatText(this.getAllText(document), formattingOptions, document.uri),
-//         ),
-//       ];
-//     } catch (e) {
-//       vscode.window.showErrorMessage('Unable to format SQL:\n' + e);
-//       return [];
-//     }
-//   }
 
-//   private getAllText(document: vscode.TextDocument) {
-//     // extract all lines from document
-//     return [...new Array(document.lineCount)].map((_, i) => document.lineAt(i).text).join('\n');
-//   }
+export default class SqlFormattingProvider implements vscode.DocumentFormattingEditProvider 
+{
+    public async Test(text: string) : Promise<string> {
+     
+        const cliPath =  '~/.config/Code/User/globalStorage/undefined_publisher.tsqlsharp/tsqlsharp/tsqlsharp-formatter';
+        var fileProvider = new FileProvider();
 
-//   private fullDocumentRange(document: vscode.TextDocument): vscode.Range {
-//     return new vscode.Range(
-//       document.positionAt(0),
-//       document.lineAt(document.lineCount - 1).range.end,
-//     );
-//   }
+	    var output =  await fileProvider.formatWithCliTool(text, cliPath);
 
-//   private formatText(text: string, formattingOptions: vscode.FormattingOptions, uri: vscode.Uri) {
-//     const extensionSettings = vscode.workspace.getConfiguration('SQL-Formatter-VSCode', uri);
-//     //const formatConfig = createConfig(extensionSettings, formattingOptions, this.language);
-//     // Need to use the CLI parts here:
-    
-//     //Sreturn formatEditorText(text, formatConfig);
-//   }
-// }
+        var parsed = JSON.parse(output);
+        return parsed.Output;
+    }
+
+
+
+  async provideDocumentFormattingEdits(
+    document: vscode.TextDocument,
+    formattingOptions: vscode.FormattingOptions,
+  ): Promise<vscode.TextEdit[]> {
+    try {
+      return [
+        vscode.TextEdit.replace(
+          this.fullDocumentRange(document),
+          await this.formatText(this.getAllText(document), document.uri),
+        ),
+      ];
+    } catch (e) {
+      vscode.window.showErrorMessage('Unable to format SQL:\n' + e);
+      return [];
+    }
+  }
+
+  private getAllText(document: vscode.TextDocument) {
+    // extract all lines from document
+    return [...new Array(document.lineCount)].map((_, i) => document.lineAt(i).text).join('\n');
+  }
+
+  private fullDocumentRange(document: vscode.TextDocument): vscode.Range {
+    return new vscode.Range(
+      document.positionAt(0),
+      document.lineAt(document.lineCount - 1).range.end,
+    );
+  }
+
+  private async formatText(text: string, uri: vscode.Uri): Promise<string> {
+    // const extensionSettings = vscode.workspace.getConfiguration('SQL-Formatter-VSCode', uri);
+    var output = this.Test(text);
+    return output;
+  }
+}
--- src/extension.ts
+++ src/extension.ts
@@ -3,7 +3,7 @@
 
 import * as vscode from 'vscode';
 import ServiceDownloadProvider, * as downlaodProvider from './ServiceDownloadProvider';
-import  FileProvider, * as fileProvider from './FileProvider';
+import SqlFormattingProvider, * as sqlFormattingProvider from './SqlFormattingProvider';
 
 // This method is called when your extension is activated
 // Your extension is activated the very first time the command is executed
@@ -18,39 +18,23 @@ export async function activate(context: vscode.ExtensionContext) {
 	const installPath = context.globalStorageUri.fsPath + '/tsqlsharp';
 	const zipFile = installPath + '/tsqlsharp-formatter-0.0.2-linux-amd64.tar.gz';
 	const cliPath = installPath + '/tsqlsharp-formatter';
-	var test1 = new ServiceDownloadProvider();
-	var testing = await test1.downloadFile(link, installPath);
-	var t = await test1.decompressTar(zipFile, installPath);
+	// var test1 = new ServiceDownloadProvider();
+	// var testing = await test1.downloadFile(link, installPath);
+	// var t = await test1.decompressTar(zipFile, installPath);
 
-	var fileProvider = new FileProvider();
+	// var sql = new SqlFormattingProvider();
+	// await sql.Test(cliPath);
+	
+	context.subscriptions.push(
+		vscode.languages.registerDocumentFormattingEditProvider(
+		'sql',
+		new SqlFormattingProvider(),
+		)
+	);
+	
 
-	var output = await fileProvider.formatWithCliTool("SELECT * FROM dbo", cliPath);
+  
 
-	const test = os. platform() === 'win32';
-	// The command has been defined in the package.json file
-	// Now provide the implementation of the command with registerCommand
-	// The commandId parameter must match the command field in package.json
-	const disposable = vscode.commands.registerCommand('tsqlsharp.helloWorld', () => {
-		// The code you place here will be executed every time your command is executed
-		// Display a message box to the user
-		vscode.window.showInformationMessage('Hello World from tsqlsharp-formatter!');
-	});
-
-	// var files = new fileProvider.FileProvider();
-
-	// var content = files.formatWithCliTool('select * from #test');
-
-	 // 👍 formatter implemented using API
-    // vscode.languages.registerDocumentFormattingEditProvider('sql', {
-    //     provideDocumentFormattingEdits(document: vscode.TextDocument): vscode.TextEdit[] {
-    //         const firstLine = document.lineAt(0);
-    //         if (firstLine.text !== '42') {
-    //             return [vscode.TextEdit.insert(firstLine.range.start, '42\n')];
-    //         }
-    //     }
-    // });
-
-		context.subscriptions.push(disposable);
 
 }
 
